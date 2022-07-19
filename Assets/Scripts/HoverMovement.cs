bb10c4c2ccd38be6fdc152018853b719a7370196
Lobinux <lobinux@digilearnnials.com>
2022-07-19 12:50:02 -0300

    Some changes in code

--- Assets/Scripts/HoverMovement.cs
+++ Assets/Scripts/HoverMovement.cs
@@ -3,95 +3,98 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
-public class HoverMovement : MonoBehaviour
+namespace GravityTanks
 {
-    [SerializeField] float multiplier;
-    [SerializeField] float moveForce, turnTorque;
-    [SerializeField] Vector3 centerOfMass = Vector3.down;
-    [SerializeField] LayerMask rayLayerMask;
-    [SerializeField] Transform[] anchors;
-
-    Rigidbody rb;
-
-    float moveInput;
-    float turnInput;
-
-    RaycastHit[] hits;
-
-    private void Awake() 
+    [RequireComponent(typeof(Rigidbody), typeof(MeshCollider))]
+    public class HoverMovement : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
+        [SerializeField] float multiplier = 2;
+        [SerializeField] float moveForce = 20, turnTorque = 10;
+        [SerializeField] Vector3 centerOfMass = Vector3.down * 2;
+        [SerializeField] LayerMask rayLayerMask;
+        [SerializeField] Transform[] anchors;
 
-        rb.mass = 1;
-        rb.drag = 1;
-        rb.angularDrag = 2;
+        Rigidbody rb;
 
-        hits = new RaycastHit[anchors.Length];
-    }
+        float moveInput;
+        float turnInput;
 
-    private void FixedUpdate()
-    {
-        rb.centerOfMass = centerOfMass;
+        RaycastHit[] hits;
 
-        for (int i = 0; i < anchors.Length; i++)
+        private void Awake()
         {
-            ApplyForce(anchors[i], ref hits[i]);
+            rb = GetComponent<Rigidbody>();
+
+            rb.mass = 1;
+            rb.drag = 1;
+            rb.angularDrag = 2;
+
+            hits = new RaycastHit[anchors.Length];
         }
 
-        rb.AddForce(moveInput * moveForce * transform.forward);
-        rb.AddTorque(turnInput * turnTorque * transform.up);
-    }
-
-    void ApplyForce(Transform anchor, ref RaycastHit hit)
-    {
-        if(Physics.Raycast(anchor.position, -anchor.up, out hit, multiplier, rayLayerMask))
+        private void FixedUpdate()
         {
-            float force = Mathf.Abs(1 / Mathf.Clamp(Vector3.Distance(hit.point, anchor.position), .1f, multiplier));
-            rb.AddForceAtPosition(transform.up * force * multiplier, anchor.position, ForceMode.Acceleration);
-        }
-    }
-
-    public void MoveInput(InputAction.CallbackContext callbackContext)
-    {
-        Vector2 input = callbackContext.ReadValue<Vector2>();
-        moveInput = input.y;
-        turnInput = input.x;
-    }
-
-#if UNITY_EDITOR
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(transform.position + centerOfMass, .25f);
-
-        if(anchors != null && anchors.Length > 0) 
-        {
-            Gizmos.color = Color.green;
+            rb.centerOfMass = centerOfMass;
 
             for (int i = 0; i < anchors.Length; i++)
             {
-                Gizmos.DrawSphere(anchors[i].position, .25f);
+                ApplyForce(anchors[i], ref hits[i]);
+            }
+
+            rb.AddForce(moveInput * moveForce * transform.forward);
+            rb.AddTorque(turnInput * turnTorque * transform.up);
+        }
+
+        void ApplyForce(Transform anchor, ref RaycastHit hit)
+        {
+            if (Physics.Raycast(anchor.position, -anchor.up, out hit, multiplier, rayLayerMask))
+            {
+                float force = Mathf.Abs(1 / Mathf.Clamp(Vector3.Distance(hit.point, anchor.position), .1f, multiplier));
+                rb.AddForceAtPosition(transform.up * force * multiplier, anchor.position, ForceMode.Acceleration);
             }
         }
 
-        if(hits != null && hits.Length > 0)
+        public void MoveInput(InputAction.CallbackContext callbackContext)
         {
-            for (int i = 0; i < hits.Length; i++)
+            Vector2 input = callbackContext.ReadValue<Vector2>();
+            moveInput = input.y;
+            turnInput = input.x;
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(transform.position + centerOfMass, .25f);
+
+            if (anchors != null && anchors.Length > 0)
             {
-                if (hits[i].distance > 0)
+                Gizmos.color = Color.green;
+
+                for (int i = 0; i < anchors.Length; i++)
                 {
-                    Gizmos.color = Color.red;
-                    Gizmos.DrawLine(anchors[i].position, hits[i].point);
-                    Gizmos.DrawWireSphere(hits[i].point, .25f);
+                    Gizmos.DrawSphere(anchors[i].position, .25f);
                 }
-                else
+            }
+
+            if (hits != null && hits.Length > 0)
+            {
+                for (int i = 0; i < hits.Length; i++)
                 {
-                    Gizmos.DrawLine(anchors[i].position, anchors[i].position - anchors[i].up * multiplier);
-                    Gizmos.DrawWireSphere(anchors[i].position - anchors[i].up * multiplier, .25f);
+                    if (hits[i].distance > 0)
+                    {
+                        Gizmos.color = Color.red;
+                        Gizmos.DrawLine(anchors[i].position, hits[i].point);
+                        Gizmos.DrawWireSphere(hits[i].point, .25f);
+                    }
+                    else
+                    {
+                        Gizmos.DrawLine(anchors[i].position, anchors[i].position - anchors[i].up * multiplier);
+                        Gizmos.DrawWireSphere(anchors[i].position - anchors[i].up * multiplier, .25f);
+                    }
                 }
             }
         }
-    }
 #endif
+    }
 }
