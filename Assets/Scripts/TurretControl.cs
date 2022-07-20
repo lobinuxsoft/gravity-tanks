81a467c5be2659e5d11da910f4c6d41cc5e168b3
Lobinux <lobinux@digilearnnials.com>
2022-07-19 21:15:40 -0300

    Implement turret rotation

---
+++ Assets/Scripts/TurretControl.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GravityTanks
+{
+    public class TurretControl : MonoBehaviour
+    {
+        [SerializeField] float rotSpeed = 1f;
+        [SerializeField] Transform turret;
+
+        bool isMouse;
+        Camera cam;
+        Vector2 pointerInput;
+        Vector3 dir;
+        Vector3 pointer3D = Vector3.zero;
+
+        private void Awake()
+        {
+            cam = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            Ray ray = cam.ViewportPointToRay(pointerInput);
+
+            Physics.Raycast(ray, out RaycastHit hit);
+
+            pointer3D = hit.point;
+
+            if(Vector3.Distance(pointer3D, transform.position) > 1.5f)
+            {
+                dir = (pointer3D - transform.position).normalized;
+            }
+
+            float singleStep = rotSpeed * Time.deltaTime;
+
+            Vector3 newDir = Vector3.RotateTowards(turret.forward, dir, singleStep, 0.0f);
+
+            turret.rotation = Quaternion.LookRotation(newDir, transform.up);
+        }
+
+        public void PointerInput(InputAction.CallbackContext callbackContext)
+        {
+            Vector2 inputValue = callbackContext.ReadValue<Vector2>();
+
+            if (isMouse)
+            {
+                pointerInput.x = inputValue.x / Screen.width;
+                pointerInput.y = inputValue.y / Screen.height;
+            }
+            else
+            {
+                pointerInput.x = .5f + (inputValue.x * .5f);
+                pointerInput.y = .5f + (inputValue.y * .5f);
+            }
+        }
+
+        Vector3 ProjectDirectionOnPlane(Vector3 direction, Vector3 normal)
+        {
+            return (direction - normal * Vector3.Dot(direction, normal)).normalized;
+        }
+
+        public void ChangedControls(PlayerInput playerInput)
+        {
+            isMouse = !playerInput.currentControlScheme.Contains("Gamepad");
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(pointer3D, .25f);
+        }
+    }
+}
