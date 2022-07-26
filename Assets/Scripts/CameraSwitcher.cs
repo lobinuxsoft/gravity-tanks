64ae2ab914f1e7ef2b13ad36c5c7101aa7e886d2
Lobinux <lobinux@digilearnnials.com>
2022-07-26 19:24:17 -0300

    Add swiching camera control

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,26 @@
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GravityTanks.Utils
+{
+    public class CameraSwitcher : MonoBehaviour
+    {
+        [SerializeField] CinemachineVirtualCamera farCamera;
+        [SerializeField] CinemachineVirtualCamera nearCamera;
+
+        void Start()
+        {
+            farCamera.gameObject.SetActive(true);
+            nearCamera.gameObject.SetActive(false);
+        }
+
+        public void SwitchCamera(InputAction.CallbackContext context)
+        {
+            if(!context.performed) return;
+
+            nearCamera.gameObject.SetActive(!nearCamera.gameObject.activeSelf);
+            farCamera.gameObject.SetActive(!farCamera.gameObject.activeSelf);
+        }
+    }
+}
