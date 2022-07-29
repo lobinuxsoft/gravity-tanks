2e92c55f691238795f1852c69ccf6ffbe8755398
Matias Galarza <lobinux@digilearnnials.com>
2022-07-29 00:29:09 -0300

    Game over scene

---
+++ Assets/Scripts/Utils/ScoreBoardData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using CryingOnionTools.ScriptableVariables;
+using System.Collections.Generic;
+
+namespace GravityTanks.Utils
+{
+    [CreateAssetMenu(menuName = "Gravity Tanks/ Utils/ Score Board Data")]
+    public class ScoreBoardData : ScriptableVariable<List<ScoreData>>
+    {
+        public override void EraseData()
+        {
+            base.EraseData();
+            value.Clear();
+        }
+    }
+}
