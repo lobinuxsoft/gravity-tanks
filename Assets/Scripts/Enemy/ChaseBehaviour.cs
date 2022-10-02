36e9d74e0cec2379891b444d6747b2552fd1d980
Lobinux <lobinux@digilearnnials.com>
2022-10-02 03:36:26 -0300

    Improve enemy IA

--- Assets/Scripts/Enemy/ChaseBehaviour.cs
+++ Assets/Scripts/Enemy/ChaseBehaviour.cs
@@ -1,43 +1,44 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace GravityTanks.Enemy.Behaviour
 {
     [CreateAssetMenu(menuName = "Enemy/Behaviour/Chase")]
     public class ChaseBehaviour : IABehaviour
     {
-        [SerializeField] private LayerMask rayLayerMask;
-        [SerializeField] private float moveForce = 10, turnSpeed = 10, heighForce = 5;
+        [SerializeField] float refreshTargetPositionRate = .25f;
+        [SerializeField] private float moveSpeed = 5, turnSpeed = 5, heighForce = 1;
 
-        HoverMovement hoverMovement;
+        private NavMeshAgent agent;
+        private Rigidbody rb;
         private Transform target;
-        private Vector3 targetDir = Vector3.zero;
 
+        private float lastRefresh;
+        
         public override void DoBehaviour(GameObject owner)
         {
-            if (!hoverMovement)
-            {
-                hoverMovement = owner.AddComponent<HoverMovement>();
-                hoverMovement.RayLayerMask = rayLayerMask;
-                hoverMovement.HeighForce = heighForce;
-                hoverMovement.MoveForce = moveForce;
-                hoverMovement.TurnSpeed = turnSpeed;
-            }
-
             if (!target) target = GameObject.FindWithTag("Player").transform;
 
-            Vector3 dir = (target.position - owner.transform.position).normalized;
+            if (!agent)
+            {
+                agent = owner.AddComponent<NavMeshAgent>();
+                agent.speed = moveSpeed;
+                agent.angularSpeed = turnSpeed;
+                agent.baseOffset = heighForce;
+            }
 
-            Vector3 forward = ProjectDirectionOnPlane(Vector3.forward, Vector3.up);
-            Vector3 right = ProjectDirectionOnPlane(Vector3.right, Vector3.up);
+            if(!rb)
+            {
+                rb = owner.GetComponent<Rigidbody>();
+                rb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+                rb.isKinematic = true;
+            }
 
-            targetDir = dir.x * right + dir.z * forward;
-
-            hoverMovement.Move(new Vector2(targetDir.x, targetDir.z));
-        }
-
-        Vector3 ProjectDirectionOnPlane(Vector3 direction, Vector3 normal)
-        {
-            return (direction - normal * Vector3.Dot(direction, normal)).normalized;
+            if(Time.time - lastRefresh < refreshTargetPositionRate)
+            {
+                lastRefresh = Time.time;
+                agent.SetDestination(target.position);
+            }
         }
     }
 }
