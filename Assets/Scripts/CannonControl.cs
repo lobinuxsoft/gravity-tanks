0bd76f7b9eb7d7bdff415297f39a0c6e98187a3c
Lobinux <lobinux@digilearnnials.com>
2022-10-11 18:21:31 -0300

    Modified CannonControl.cs

--- Assets/Scripts/CannonControl.cs
+++ Assets/Scripts/CannonControl.cs
@@ -7,6 +7,8 @@ namespace GravityTanks
     [RequireComponent(typeof(Damager), typeof(SFXTrigger))]
     public class CannonControl : MonoBehaviour
     {
+        [SerializeField] int emmitAmount = 1;
+        [SerializeField] float arcShootAngle = 45;
         [SerializeField] LayerMask layerToCollide;
         [SerializeField] AudioClip shootSfx;
         [SerializeField] AudioClip impactSfx;
@@ -16,6 +18,7 @@ namespace GravityTanks
 
         ParticleSystem particle;
         ParticleSystem.CollisionModule collisionModule;
+        ParticleSystem.ShapeModule shapeModule;
 
         SFXTrigger sfxTrigger;
 
@@ -26,6 +29,17 @@ namespace GravityTanks
             particle = GetComponent<ParticleSystem>();
             collisionModule = particle.collision;
             collisionModule.collidesWith = layerToCollide;
+            collisionModule.sendCollisionMessages = true;
+
+            shapeModule = particle.shape;
+            shapeModule.arc = arcShootAngle;
+            shapeModule.radius = 0;
+            shapeModule.radiusThickness = 0;
+            shapeModule.arcMode = ParticleSystemShapeMultiModeValue.BurstSpread;
+            shapeModule.arcSpread = 0;
+            shapeModule.shapeType = ParticleSystemShapeType.Circle;
+            shapeModule.rotation = new Vector3(90f, -90f + (shapeModule.arc / 2), 0f);
+
             damager = GetComponent<Damager>();
             sfxTrigger = GetComponent<SFXTrigger>();
         }
@@ -39,7 +53,7 @@ namespace GravityTanks
         IEnumerator ShotRoutine(float delay)
         {
             sfxTrigger.PlaySFX(shootSfx);
-            particle.Emit(1);
+            particle.Emit(emmitAmount);
             yield return new WaitForSeconds(delay);
             shotRoutine = null;
         }
